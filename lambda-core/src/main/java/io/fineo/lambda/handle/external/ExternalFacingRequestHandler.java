edb9df5732122b4129d0e217934615b10d3d3e76
Jesse Yates <jesse.k.yates@gmail.com>
2016-09-09 10:44:37 -0700

    Slightly better unexpected error handling for external errrors

--- lambda-core/src/main/java/io/fineo/lambda/handle/external/ExternalFacingRequestHandler.java
+++ lambda-core/src/main/java/io/fineo/lambda/handle/external/ExternalFacingRequestHandler.java
@@ -34,12 +34,15 @@ public abstract class ExternalFacingRequestHandler<INPUT, OUTPUT>
     String msg = e.getMessage();
     // wrap the exception into better error handling
     if (msg == null || !msg.startsWith("{")) {
-      StringBuffer sb = new StringBuffer("Internal Server Error. Please send this output "
-                                         + "to errors@fineo.io\n Error:\n");
+      StringBuffer sb = new StringBuffer(
+        "Internal Server Error. Please send this output to errors@fineo.io\n Error:\n ");
+      sb.append(e.getClass().getName());
+      sb.append("\n");
       if (msg != null) {
+        sb.append("Message: ");
         sb.append(msg);
-        sb.append("\n\nStack Trace:");
       }
+      sb.append("\n\nStack Trace:");
       try {
         Joiner.on("\n").appendTo(sb, e.getStackTrace());
         return ExternalErrorsUtil.get500(context, sb.toString());
