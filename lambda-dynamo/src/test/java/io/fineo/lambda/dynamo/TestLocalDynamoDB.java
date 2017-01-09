ff5d3af5d128d10fed06a7a6808b1810288b085f
Jesse Yates <jesse@fineo.io>
2017-01-08 16:12:16 -0800

    Cleaning up dynamodb local info

--- lambda-dynamo/src/test/java/io/fineo/lambda/dynamo/TestLocalDynamoDB.java
+++ lambda-dynamo/src/test/java/io/fineo/lambda/dynamo/TestLocalDynamoDB.java
@@ -7,8 +7,10 @@ import org.junit.Test;
  * Simple test to run dynamodb locally.
  * Important things you need to ensure you can use dynamodb local:
  * <ol>
- *   <li>-Dsqlite4java.library.path=${project.build.directory}/dependencies in you surefire
- *   argline settings</li>
+ * <li>-Dsqlite4java.library.path=${project.build.directory}/dependencies in you surefire
+ * argline settings</li>
+ * <li>maven-dependency-plugin to copy all the test dependencies (needed for sql lib path</li>
+ * <li>dynamo s3 release repository</li>
  * </ol>
  * Otherwise, just use the same dependencies we have here.
  */
